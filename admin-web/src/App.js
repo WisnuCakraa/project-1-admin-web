66df5767953b6c6af950d5a359708f4dcd89bf10
WisnuCakraa <you@example.com>
2021-01-27 10:33:00 +0700

    [ADD]Route Structure

--- admin-web/src/App.js
+++ admin-web/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div>
-        <Dashboard />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+        </Switch>
+      </Router>
     );
   }
 }
+
+export default App;

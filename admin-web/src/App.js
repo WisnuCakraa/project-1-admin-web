066b3cdda10d93d8e578b6ae11d872e6ba443fb4
WisnuCakraa <you@example.com>
2021-01-27 10:17:23 +0700

    [ADD] Structure Folder

---
+++ admin-web/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Dashboard from "./Components/Dashboard/Dashboard";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dashboard />
+      </div>
+    );
+  }
+}

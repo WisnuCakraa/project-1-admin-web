40c2429b4d6763fd40e9c160c63db0f73abe84f0
WisnuCakraa <you@example.com>
2021-02-03 15:14:50 +0700

    commit

---
+++ admin-web/src/Components/Product/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MainContent from "../MainContent/MainContent";
+export default function Product() {
+  return (
+    <div>
+      <MainContent>
+        <h1>PRODUCT HERE !</h1>
+      </MainContent>
+    </div>
+  );
+}
